e7b3eb8d25724fed54a64408dd736b57e738282d
Bhushan Wanjari <bhushanwanjari21z@gmail.com>
2022-10-19 21:37:09 +0530

    MusiWeen

---
+++ app.js
@@ -0,0 +1,19 @@
+const express= require('express');
+const bodyparser=require('body-parser');
+const request= require('request');
+const https=require('https');
+const { status } = require('express/lib/response');
+const app= express();
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended:true}));
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+})
+
+
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("server is ruuning");
+})
